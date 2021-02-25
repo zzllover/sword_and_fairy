24093efea2bf282db3ffbd889255516fd1821bcf
Ximing Peng <ximing.peng@shopee.com>
2021-02-25 14:32:06 +0800

    fix: test editor

--- demos/react-apollo-grapgql/src/App.tsx
+++ demos/react-apollo-grapgql/src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import JiraEditor from "react-jira-editor";
 import './App.css';
 
 function App() {
+
+  function onJIRAEditor_Change(editorContents: any) {
+    console.log(editorContents.text);
+    console.log(editorContents.html);
+    console.log(editorContents.markup);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +17,9 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
       </header>
+      <div>
+        <JiraEditor onChange={onJIRAEditor_Change} />
+      </div>
     </div>
   );
 }
